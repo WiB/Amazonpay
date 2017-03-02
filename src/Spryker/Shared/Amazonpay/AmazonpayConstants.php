23139d75e7f8807c282093467aa4e82f87f6f2b6
Dmitri Kadykov <dmitri.kadykov@spryker.com>
2017-03-02 13:53:19 +0000

    eco-324 config cnstants and buton template

--- src/Spryker/Shared/Amazonpay/AmazonpayConstants.php
+++ src/Spryker/Shared/Amazonpay/AmazonpayConstants.php
@@ -3,5 +3,13 @@ namespace Spryker\Shared\Amazonpay;
 
 interface AmazonpayConstants
 {
+    const ACCESS_KEY_ID = 'ACCESS_KEY_ID';
+    const CLIENT_ID = 'CLIENT_ID';
+    const SELLER_ID = 'SELLER_ID';
+    const SECRET_ACCESS_KEY = 'SECRET_ACCESS_KEY';
+    const CLIENT_SECRET = 'CLIENT_SECRET';
 
+    const PAYMENTS_API_ENDPOINT = 'PAYMENTS_API_ENDPOINT';
+    const LOGIN_API_URL = 'LOGIN_API_URL';
+    const PROFILE_API_URL = 'PROFILE_API_URL';
 }
