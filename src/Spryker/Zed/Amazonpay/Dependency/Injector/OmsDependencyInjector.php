ae98e7bea0cc85483a0e96ed54476755f4b9228d
Dmitri Kadykov <dmitri.kadykov@spryker.com>
2017-03-29 15:32:06 +0200

    ECO-334 Code Cleanup

---
+++ src/Spryker/Zed/Amazonpay/Dependency/Injector/OmsDependencyInjector.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Spryker\Zed\Amazonpay\Dependency\Injector;
+
+use Spryker\Zed\Amazonpay\Communication\Plugin\Oms\Command\CloseOrderCommandPlugin;
+use Spryker\Zed\Amazonpay\Communication\Plugin\Oms\Command\RefundOrderCommandPlugin;
+use Spryker\Zed\Amazonpay\Communication\Plugin\Oms\Condition\IsClosedConditionPlugin;
+use Spryker\Zed\Amazonpay\Communication\Plugin\Oms\Condition\IsRefundedConditionPlugin;
+use Spryker\Zed\Kernel\Container;
+use Spryker\Zed\Kernel\Dependency\Injector\AbstractDependencyInjector;
+use Spryker\Zed\Oms\Communication\Plugin\Oms\Command\CommandCollectionInterface;
+use Spryker\Zed\Oms\Communication\Plugin\Oms\Condition\ConditionCollectionInterface;
+use Spryker\Zed\Oms\OmsDependencyProvider;
+
+class OmsDependencyInjector extends AbstractDependencyInjector
+{
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
+    public function injectBusinessLayerDependencies(Container $container)
+    {
+        $container = $this->injectCommands($container);
+        $container = $this->injectConditions($container);
+
+        return $container;
+    }
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
+    protected function injectCommands(Container $container)
+    {
+        $container->extend(OmsDependencyProvider::COMMAND_PLUGINS, function (CommandCollectionInterface $commandCollection) {
+            $commandCollection
+                ->add(new CloseOrderCommandPlugin(), 'Amazonpay/CloseOrder')
+                ->add(new RefundOrderCommandPlugin(), 'Amazonpay/RefundOrder');
+
+            return $commandCollection;
+        });
+
+        return $container;
+    }
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
+    protected function injectConditions(Container $container)
+    {
+        $container->extend(OmsDependencyProvider::CONDITION_PLUGINS, function (ConditionCollectionInterface $conditionCollection) {
+            $conditionCollection
+                ->add(new IsClosedConditionPlugin(), 'Amazonpay/IsClosed')
+                ->add(new IsRefundedConditionPlugin(), 'Amazonpay/IsRefunded');
+
+            return $conditionCollection;
+        });
+
+        return $container;
+    }
+
+}
